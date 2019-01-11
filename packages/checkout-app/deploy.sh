5cac4d850c2e9f6f59b7f6d6ae2a2016e8a35129
valtermartins1301 <valtermartins.1301@gmail.com>
2019-01-11 18:20:39 -0200

    add deploy script

---
+++ packages/checkout-app/deploy.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+if [ ! -z $1 ]; then
+  export AWS_DEFAULT_PROFILE=$1
+fi
+
+FOLDER=$(pwd)
+
+cd ../../terraform
+BUCKET_NAME=$(terraform output checkout_bucket_name)
+CLOUDFRONT_ID=$(terraform output cloudfront_id)
+CLOUDFRONT_DOMAIN=$(terraform output cloudfront_domain)
+
+echo $BUCKET_NAME
+
+cd $FOLDER
+
+npm run build
+
+aws s3 sync ./dist s3://$BUCKET_NAME \
+  --delete
+
+aws cloudfront create-invalidation \
+  --paths "/*" \
+  --distribution-id $CLOUDFRONT_ID
+
+echo "=> Deployed to $CLOUDFRONT_DOMAIN"
