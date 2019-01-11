9bb7e74664273c107bb36df22a48534a8dd6b846
valtermartins1301 <valtermartins.1301@gmail.com>
2019-01-11 19:32:37 -0200

    create lambda proxy

---
+++ cloud/proxy.js
@@ -0,0 +1,51 @@
+const path = require('path');
+
+exports.handler = (evt, ctx, cb) => {
+  const {request} = evt.Records[0].cf;
+  const calendar_bucket_name = 'study-singlespa20190111195059208900000001.s3.amazonaws.com';
+  const checkout_bucket_name = 'study-singlespa20190111195059209200000002.s3.amazonaws.com';
+  const dashboard_bucket_name = 'study-singlespa20190111195059210800000003.s3.amazonaws.com';
+  const nav_bucket_name = 'study-singlespa20190111193311289500000001.s3.amazonaws.com';
+
+  let newRequest = request;
+
+  if (!path.extname(request.uri)) {
+    request.uri = '/index.html';
+  }
+
+  redirect('/calendar', calendar_bucket_name, newRequest);
+  redirect('/checkout', checkout_bucket_name, newRequest);
+  redirect('/dashboard', dashboard_bucket_name, newRequest);
+  redirect('/nav', nav_bucket_name, newRequest);
+
+  // if (/^\/nav/.test(request.uri)) {
+  //   request.uri = request.uri.replace('/nav', '');
+  //
+  //   request.origin.s3.domainName = nav_bucket_name;
+  //   request.headers['host'] = [{key: 'host', value: nav_bucket_name}];
+  //
+  //
+  //
+  //   return cb(null, request);
+  // }
+
+  console.log(newRequest);
+  cb(null, newRequest);
+}
+
+function redirect(pathname, bucket, request) {
+  const regex = new RegExp('^' + pathname);
+
+  if (!regex.test(request.uri)) {
+    return request;
+  }
+
+  request.uri = request.uri.replace(pathname, '');
+
+  request.origin.s3.domainName = bucket;
+  request.headers['host'] = [{key: 'host', value: bucket}];
+
+  console.log(pathname, request);
+
+  return request;
+}
--- terraform/cloudfront.tf
+++ terraform/cloudfront.tf
@@ -29,6 +29,10 @@ resource "aws_cloudfront_distribution" "main" {
         forward = "none"
       }
     }
+    lambda_function_association {
+      event_type = "origin-request"
+      lambda_arn = "${aws_lambda_function.proxy.qualified_arn}"
+    }
   }
   viewer_certificate {
     cloudfront_default_certificate = true
